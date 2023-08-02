d8767536abb9ea722d7223937ed5f93929fb4c9f
Siddhartha Purwar <siddpurwar@gmail.com>
2023-08-02 23:24:51 +0530

    Kaggle Notebook | utility | Version 14

--- utility.py
+++ utility.py
@@ -38,7 +38,53 @@ def run(model, optimizer, criterion, data_loader, is_cuda, mode):
     
     return epoch_loss/ len(data_loader.dataset)
 
+
+def test_run(model, criterion, data_loader, is_cuda):
+    class_correct = list(0 for i in range(10))
+    class_total = list(0 for i in range(10))
+    test_loss = 0
+    correct = 0
+    total = 0
+    
+    if is_cuda:
+        model.cuda()
         
+    with torch.no_grad():
+        model.eval()
+        
+    preds = list()
+    actuals = list()
+    
+    for batch_idx, (data, target) in tqdm(
+            enumerate(data_loader),
+            desc='Testing',
+            total=len(data_loader),
+            leave=True,
+            ncols=80
+        ):
+        
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+            
+        output = model(data)
+        loss_value = criterion(output, target).detach()
+        test_loss += loss_value*data.size(0)
+        
+        _, pred = torch.max(output, 1)
+        pred = pred.cpu().numpy().tolist()
+        preds.extend(pred)
+        actuals.extend(target.cpu().numpy().tolist())
+
+        
+        for i in range(target.shape[0]):
+            label = target[i]
+            class_correct[label] += (1 if pred[i] == label else 0)
+            class_total[label] += 1
+    
+    test_loss = test_loss/len(test_loader.dataset)
+    
+    return test_loss, preds, actuals, class_correct, class_totat
+
 
 def plot_loss(losses, epochs):
     plt.figure(figsize=(8, 5))
